736a9d4a47ce21197e5f85f48b83a86e6cf34036
viveksingh10 <viveksingh9780@gmail.com>
2020-11-14 23:39:14 +0530

    added adminpanel

---
+++ src/Admin/AdminPanelController.java
@@ -0,0 +1,112 @@
+package Admin;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class AdminPanelController {
+
+		@FXML
+		private Label adminLBL;
+
+		
+		public void GetAdmin(String admin) {
+			
+			adminLBL.setText(admin);
+
+	   }
+		
+		 public void exxit(ActionEvent event) {
+				System.exit(0);	
+				}
+		
+		public void out(ActionEvent event) {
+			try {
+				
+				((Node)event.getSource()).getScene().getWindow().hide();
+				Stage primaryStage = new Stage();
+				FXMLLoader loader = new FXMLLoader();
+				Pane root = loader.load(getClass().getResource("/Frontend/Login.fxml").openStream());
+				Scene scene = new Scene(root);
+				primaryStage.setScene(scene);
+				primaryStage.setTitle(" Login ");
+				primaryStage.show();
+				
+			} catch (Exception e) {
+				
+			}
+		}
+		
+		public void out1(ActionEvent event) {
+			try {
+				
+				((Node)event.getSource()).getScene().getWindow().hide();
+				Stage primaryStage = new Stage();
+				FXMLLoader loader = new FXMLLoader();
+				Pane root = loader.load(getClass().getResource("/Frontend/Depositwithraw.fxml").openStream());
+				Scene scene = new Scene(root);
+				primaryStage.setScene(scene);
+				primaryStage.setTitle(" New Payments ");
+				primaryStage.show();
+				
+			} catch (Exception e) {
+				
+			}
+		}
+		
+		public void out2(ActionEvent event) {
+			try {
+				
+				((Node)event.getSource()).getScene().getWindow().hide();
+				Stage primaryStage = new Stage();
+				FXMLLoader loader = new FXMLLoader();
+				Pane root = loader.load(getClass().getResource("/Frontend/Users.fxml").openStream());
+				Scene scene = new Scene(root);
+				primaryStage.setScene(scene);
+				primaryStage.setTitle(" Pending Bills ");
+				primaryStage.show();
+				
+			} catch (Exception e) {
+				
+			}
+		}
+		
+		public void out3(ActionEvent event) {
+			try {
+				
+				((Node)event.getSource()).getScene().getWindow().hide();
+				Stage primaryStage = new Stage();
+				FXMLLoader loader = new FXMLLoader();
+				Pane root = loader.load(getClass().getResource("/Frontend/loans.fxml").openStream());
+				Scene scene = new Scene(root);
+				primaryStage.setScene(scene);
+				primaryStage.setTitle(" New Bills ");
+				primaryStage.show();
+				
+			} catch (Exception e) {
+				
+			}
+		}
+		
+		public void out4(ActionEvent event) {
+			try {
+				
+				((Node)event.getSource()).getScene().getWindow().hide();
+				Stage primaryStage = new Stage();
+				FXMLLoader loader = new FXMLLoader();
+				Pane root = loader.load(getClass().getResource("/Frontend/AdminComplaints.fxml").openStream());
+				Scene scene = new Scene(root);
+				primaryStage.setScene(scene);
+				primaryStage.setTitle(" Admin Complaints ");
+				primaryStage.show();
+				
+			} catch (Exception e) {
+				
+			}
+		}
+		
+	}
