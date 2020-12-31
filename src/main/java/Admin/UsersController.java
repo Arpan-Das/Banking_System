774f022b6585019027f4e4f6d9bc88074dce465d
~Arpan <48158071+Arpan-Das@users.noreply.github.com>
2020-12-31 23:31:57 +0530

    admin_panel_user-section -> add user error fixed

--- src/main/java/Admin/UsersController.java
+++ src/main/java/Admin/UsersController.java
@@ -96,81 +96,97 @@ public class UsersController implements Initializable  {
     Statement stmt;
     
     public void Add_user(){
-    	
-    	//*************************** new account created by admin ****************************************
-    	
-    	if(!(txt_name.getText().trim().isEmpty() && txt_gender.getText().trim().isEmpty()&& txt_id.getText().trim().isEmpty() && txt_email.getText().trim().isEmpty() && txt_username.getText().trim().isEmpty())){
-    		conn = sqlconnect.dbconnect();
-    		SplitTheName.split(txt_name.getText());
-    		try {
-    			ps = conn.prepareStatement("insert into user(firstname, lastname,gender, dob, id, address, emailid, mobileno,username,password, datetime) values(?,?,?,?,?,?,?,?,?,?, datetime('now','localtime'))");
-			
-    			ps.setString(1, SplitTheName.getFirstname());
-    			ps.setString(2, SplitTheName.getLastname());
-    			ps.setString(3, txt_gender.getText());
-    			ps.setString(4, "Nil");
-    			ps.setString(5, txt_id.getText());
-    			ps.setString(6, "Nil");
-    			ps.setString(7, txt_email.getText());
-    			ps.setString(8, txt_mnumber.getText());
-    			ps.setString(9, txt_username.getText());
-    			ps.setString(10, "0000");
-			
-    			ps.executeUpdate();
-			
-    			//************** both the tables are created here i.e, user balance table and user txn table
-    			try {
-    				ps = conn.prepareStatement("select * from user where username = ?");
-    				ps.setString(1, txt_username.getText());
-    				rs = ps.executeQuery();
-    				int accno = rs.getInt("accno");
-			
-    				// create table balance
-    				String tablebalance ="CREATE TABLE IF NOT EXISTS " + txt_username.getText()+accno + " (balance real default 0.0);";
-    				try {
-    					Statement stmt = conn.createStatement();
-    					stmt.execute(tablebalance);
-    					stmt.execute("insert into " + txt_username.getText()+accno + "  values(0.0)");
-				
-    				} catch (SQLException e) {
-    					
-    					JOptionPane.showMessageDialog(null, e);
-    				}
-			
-    				// create table trx
-    				String tabletrx ="create table IF NOT EXISTS " + "trx"+txt_username.getText()+accno + " (date text, remarks text, type text, amount real, balance real)";
-    				try {
-    					Statement stmt = conn.createStatement();
-    					stmt.execute(tabletrx);
-				
-    				} catch (SQLException e) {
-    					
-    					JOptionPane.showMessageDialog(null, e);
-    				}
-			
-    			}catch(Exception e) {
-    				JOptionPane.showMessageDialog(null, "inside create table"+e);
-    			}
-			
-    			JOptionPane.showMessageDialog(null, "User Account created. Note user Default password is '0000'");
-			
-    			Update();
-			
-    			txt_id.setText("");
-    			txt_username.setText("");
-    			txt_gender.setText("");
-    			txt_email.setText("");
-    			txt_name.setText("");
-    			txt_mnumber.setText("");
-    			txt_anumber.setText("");
-    			conn.close();
-    		} catch (SQLException e) {
+    	if(txt_anumber.getText().trim().isEmpty()){
+
+        	//*************************** new account created by admin ****************************************
+        	
+        	if(!(txt_name.getText().trim().isEmpty() && txt_gender.getText().trim().isEmpty()&& txt_id.getText().trim().isEmpty() && txt_email.getText().trim().isEmpty() && txt_username.getText().trim().isEmpty())){
+        		conn = sqlconnect.dbconnect();
+        		SplitTheName.split(txt_name.getText());
+        		try {
+        			ps = conn.prepareStatement("insert into user(firstname, lastname,gender, dob, id, address, emailid, mobileno,username,password, datetime) values(?,?,?,?,?,?,?,?,?,?, datetime('now','localtime'))");
     			
-    			JOptionPane.showMessageDialog(null, e);
-    		}
-    	}else {
-    		JOptionPane.showMessageDialog(null,"Please enter all the details");
+        			ps.setString(1, SplitTheName.getFirstname());
+        			ps.setString(2, SplitTheName.getLastname());
+        			ps.setString(3, txt_gender.getText());
+        			ps.setString(4, "Nil");
+        			ps.setString(5, txt_id.getText());
+        			ps.setString(6, "Nil");
+        			ps.setString(7, txt_email.getText());
+        			ps.setString(8, txt_mnumber.getText());
+        			ps.setString(9, txt_username.getText());
+        			ps.setString(10, "0000");
+    			
+        			ps.executeUpdate();
+    			
+        			//************** both the tables are created here i.e, user balance table and user txn table
+        			try {
+        				ps = conn.prepareStatement("select * from user where username = ?");
+        				ps.setString(1, txt_username.getText());
+        				rs = ps.executeQuery();
+        				int accno = rs.getInt("accno");
+    			
+        				// create table balance
+        				String tablebalance ="CREATE TABLE IF NOT EXISTS " + txt_username.getText()+accno + " (balance real default 0.0);";
+        				try {
+        					Statement stmt = conn.createStatement();
+        					stmt.execute(tablebalance);
+        					stmt.execute("insert into " + txt_username.getText()+accno + "  values(0.0)");
+    				
+        				} catch (SQLException e) {
+        					
+        					JOptionPane.showMessageDialog(null, e);
+        				}
+    			
+        				// create table trx
+        				String tabletrx ="create table IF NOT EXISTS " + "trx"+txt_username.getText()+accno + " (date text, remarks text, type text, amount real, balance real)";
+        				try {
+        					Statement stmt = conn.createStatement();
+        					stmt.execute(tabletrx);
+    				
+        				} catch (SQLException e) {
+        					
+        					JOptionPane.showMessageDialog(null, e);
+        				}
+    			
+        			}catch(Exception e) {
+        				JOptionPane.showMessageDialog(null, "inside create table"+e);
+        			}
+    			
+        			JOptionPane.showMessageDialog(null, "User Account created. Note user Default password is '0000'");
+    			
+        			Update();
+    			
+        			txt_id.setText("");
+        			txt_username.setText("");
+        			txt_gender.setText("");
+        			txt_email.setText("");
+        			txt_name.setText("");
+        			txt_mnumber.setText("");
+        			txt_anumber.setText("");
+        			conn.close();
+        		} catch (SQLException e) {
+        			
+        			JOptionPane.showMessageDialog(null, e);
+        		}
+        	}else {
+        		JOptionPane.showMessageDialog(null,"Please enter all the details, except Account No.(it will auto-generate).");
+        	}
+    	}else{
+    		JOptionPane.showMessageDialog(null, "Please enter new details by clearing the previous data using clear button before adding new user.");
     	}
+    	
+    }
+    
+    public void clear() {
+    	txt_anumber.setText(null);
+    	txt_name.setText(null);
+    	txt_gender.setText(null);
+    	txt_email.setText(null);
+    	txt_id.setText(null);
+    	txt_mnumber.setText(null);
+    	txt_username.setText(null);
+    
     }
     
     public void getSelected(MouseEvent event) {
